ddf9d15241ff531809261292c7a590c8f195b5dd
james-m-thomas <james.m.thomas@getcruise.com>
2023-06-10 00:06:37 -0400

    Cleanup

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,12 +5,6 @@ provider "google" {
   credentials = var.gcp_credentials
 }
 
-resource "google_storage_bucket" "example_gcs_bucket" {
-  name          = "self-2023-test-bucket"
-  location      = "US"
-  force_destroy = true
-}
-
 variable "gcp_credentials" {
   type      = string
   sensitive = true
