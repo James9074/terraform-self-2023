a495675dd9e759bb5926b129138b4c1123aa26ab
james-m-thomas <james.m.thomas@getcruise.com>
2023-06-09 23:47:07 -0400

    Adding Google Credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 provider "google" {
-  project = "terraform-self-2023"
-  region  = "us-east1"
-  zone    = "us-east1-b"
+  project     = "terraform-self-2023"
+  region      = "us-east1"
+  zone        = "us-east1-b"
+  credentials = vars.GOOGLE_CREDENTIALS
+}
+
+variable "GOOGLE_CREDENTIALS" {
+  type = string
 }
