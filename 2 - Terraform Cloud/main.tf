1f63e86508c56a9be0df76d055f9e37e06cf7ebe
James Thomas <james9074@gmail.com>
2023-06-11 09:24:39 -0400

    Update main.tf

--- 2 - Terraform Cloud/main.tf
+++ 2 - Terraform Cloud/main.tf
@@ -4,32 +4,3 @@ provider "google" {
   zone        = "us-east1-b"
   credentials = var.gcp_credentials
 }
-
-resource "google_storage_bucket" "test_bucket" {
-  name          = "self-2023-tf-cloud-test-bucket"
-  location      = "US"
-  force_destroy = true
-}
-
-
-resource "google_compute_instance" "amazing_virtual_machine" {
-  count        = 1
-  name         = "example-vm-instance"
-  machine_type = "f1-micro"
-  zone         = "us-east1-b"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-10"
-    }
-  }
-
-  network_interface {
-    network = "default"
-  }
-}
-
-variable "gcp_credentials" {
-  type      = string
-  sensitive = true
-}
